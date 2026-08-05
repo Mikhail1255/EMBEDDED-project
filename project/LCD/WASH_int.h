84dd5464fd4c4897e0e7f6c363f5de9d4a9c93fc
Mikhail1255 <mekhaeelmedhat@gmail.com>
2026-08-05 09:49:07 +0300

    Add files via upload

---
+++ project/LCD/WASH_int.h
@@ -0,0 +1,12 @@
+/************************************************/
+/********* File  : WASH_int.h     ***************/
+/************************************************/
+
+#ifndef WASH_INT_H
+#define WASH_INT_H
+
+/* Function Prototypes */
+void WASH_voidGetPreferences(u8 *ModeVariable, u8 *SpeedVariable);
+void WASH_voidRunCycle(u8 SelectedSpeed);
+
+#endif
